d4e04130f34522d97593f3de11311f3f0c4b83cb
adCerros <adriancerrossanchez@gmail.com>
2021-09-18 15:46:42 +0200

    Ahora el normalizador es por filas, se almacena aparte el de columnas

--- parser2.py
+++ parser2.py
@@ -33,27 +33,21 @@ class dataPreparator:
             # print(floatLine)
         return data
 
-
-    # Normalizador de datos universal
     def numberNormalizer(self, data):
         numberOfRows = len(data)
         numberOfColumns = len(data[0])
-        listOfMax = []
-        listOfMin = []
-        # Busqueda de maximos y minimos recorriendo la matriz por columnas
-        for j in range(numberOfColumns):
-            # Se inicializan los valores con el primer elemento de la columna en cada iteraccion
-            max = data[0][j]
-            min = data[0][j]
-            for i in range(numberOfRows):
+        # Se inicializan los valores
+        listOfMax = list(data[0])
+        listOfMin = list(data[0])
+        # Busqueda de maximos y minimos
+        for i in range(numberOfRows):
+            for j in range(numberOfColumns):
                 currentElement = data[i][j]
-                if currentElement > max:
-                    max = currentElement
-                if currentElement < min:
-                    min = currentElement
-            # Cada elemento de la lista representa el min/max de cada columna de la lista global
-            listOfMax.append(max)
-            listOfMin.append(min)
+                if currentElement > listOfMax[j]:
+                    listOfMax[j] = currentElement
+                if currentElement < listOfMin[j]:
+                    listOfMin[j] = currentElement
+        # Normalizacion
         for i in range(numberOfRows):
             for j in range(numberOfColumns):
                 data[i][j] = (data[i][j] - listOfMin[j])/(listOfMax[j]-listOfMin[j])
